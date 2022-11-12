16dea4af0c0837582eb65e49410e7e2fe103001b
flo <inbox.733@gmail.com>
2022-11-12 09:19:49 -0700

    Imported realworld backend app.

---
+++ actix-realworld-example-app/src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate failure;
+extern crate jsonwebtoken as jwt;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
+mod app;
+mod db;
+mod error;
+mod models;
+mod prelude;
+mod schema;
+mod utils;
+
+use std::env;
+
+#[actix_web::main] 
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    if env::var("RUST_LOG").ok().is_none() {
+        env::set_var("RUST_LOG", "conduit=debug,actix_web=info");
+    }
+    env_logger::init();
+
+    app::start().await
+}
