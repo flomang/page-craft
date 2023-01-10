2c890c7359b875ca294e2c11c67b7d147331d052
flo <inbox.733@gmail.com>
2023-01-10 13:35:12 -0700

    Added new actix-web graphql backend using async-graphql startwars template as base.

---
+++ actix-backend-graphql/src/main.rs
@@ -0,0 +1,35 @@
+use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Result};
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+
+mod starwars;
+use starwars::{QueryRoot, StarWars, StarWarsSchema};
+
+async fn index(schema: web::Data<StarWarsSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_graphiql() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(GraphiQLSource::build().endpoint("/").finish()))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(StarWars::new())
+        .finish();
+
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(schema.clone()))
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
